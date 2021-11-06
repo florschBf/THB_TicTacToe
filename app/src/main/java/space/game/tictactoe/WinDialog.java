3658422533eee1637d398741feeaa557752177e2
MagnificentLady <MagnificentLady@protonmail.com>
2021-11-06 11:59:24 +0100

    Dialogfenster win, lose, draw, in externe Klasse ausgelagert

---
+++ app/src/main/java/space/game/tictactoe/WinDialog.java
@@ -0,0 +1,52 @@
+package space.game.tictactoe;
+
+import static android.graphics.Color.TRANSPARENT;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+
+public class WinDialog extends Dialog {
+
+    private final GameActivity gameActivity;
+
+    public WinDialog(@NonNull Context context, GameActivity gameActivity) {
+        super(context);
+        this.gameActivity = gameActivity;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView((R.layout.layout_dialog_win));
+        getWindow().setBackgroundDrawable(new ColorDrawable(TRANSPARENT));
+
+        final ImageView imageViewClose = findViewById(R.id.imageViewClose);
+        final Button btnPlay = findViewById(R.id.btn_play);
+        final Button btnMenu = findViewById(R.id.btn_menu);
+
+        imageViewClose.setOnClickListener(v -> {
+//            gameActivity.ResetGame();
+            dismiss();
+        });
+
+        btnPlay.setOnClickListener(v -> {
+            gameActivity.ResetGame();
+            dismiss();
+
+        });
+        btnMenu.setOnClickListener(v -> {
+            Intent intent = new Intent(this.gameActivity, MenuActivity.class);
+            gameActivity.startActivity(intent);
+            dismiss();
+        });
+
+    }
+}
